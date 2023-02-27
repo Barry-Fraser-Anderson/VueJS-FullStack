24f93f025e73e9033bdbed66d3860f7d800168b6
Barry Anderson <barryanderson60@outlook.com>
2023-02-27 23:55:06 +0000

    Added Not Found Page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import CartPage from '../views/CartPage.vue';
 import ProductDetailPage from '../views/ProductDetailPage.vue';
 import ProductsPage from '../views/ProductsPage.vue';
+import NotFoundPage from '../views/NotFoundPage.vue';
 
 const routes = [
   {
@@ -22,6 +23,10 @@ const routes = [
   {
     path: '/',
     redirect: '/products',
+  },
+  {
+    path: '/:pathmatch(.*)*',
+    component: NotFoundPage,
   },
 ];
 
